b20d0c845c2d13fd8f3956bd3ecf02f4d1e07a63
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2020-12-03 23:38:55 +0300

    clean ups

--- src/hash_table.rs
+++ src/hash_table.rs
@@ -3,34 +3,31 @@
 //! An idea is very simple. We put values in the array, but not just by pushing them. We have some smart algorithm for pushing,
 //! which requires having a way to compute the position of inserting value. The "way" is actually a hash function. Look at `HashTable::eval_index` method.
 
+// todo обозначь про контракт между cap и len
+
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 use std::mem::replace;
 
 const INITIAL_LEN: usize = 2;
 
-pub(super) struct HashTable<K, V>
-where
-    K: Hash + Eq,
-{
-    // Not just V, but (K, V), because if we have faced collision and
-    // there's more than 1 element in vector, then we need to some how recognize
-    // desired value.
-    //
-    // Let's just use Vec<Vec> instead of Vec<LinkedList>.
-    buckets: Vec<Vec<(K, V)>>,
+pub(super) struct HashTable<K: Hash + Eq, V> {
+    buckets: Buckets<K, V>,
 }
 
-impl<K, V> HashTable<K, V>
-where
-    K: Hash + Eq,
-{
+// Not just V, but (K, V), because if we have faced collision and
+// there's more than 1 element in vector, then we need to some how recognize
+// desired value.
+//
+// Let's just use Vec<Vec> instead of Vec<LinkedList>.
+struct Buckets<K: Hash + Eq, V>(Vec<BucketInner<K, V>>);
+
+type BucketInner<K, V> = Vec<(K, V)>;
+
+impl<K: Hash + Eq, V> HashTable<K, V> {
     pub(super) fn new() -> Self {
-        // Initialize with len equal to cap
-        // Initializing like this `vec![Vec::new(), some_cap] needs K to implement Clone`,
-        // but by doing current zero initialization and `self.resize()` on first insert we enlarge our trait bound.
         HashTable {
-            buckets: Vec::new(),
+            buckets: Buckets::new(),
         }
     }
 
@@ -38,60 +35,33 @@ where
         if self.needs_resize() {
             self.resize()
         }
-
-        let index = self.eval_index(&key);
-        // Checking if value exists by finding its index in `self.bucket[index]` and returning it
-        if let Some(pos) = self.buckets[index].iter().position(|(k, _)| k == &key) {
-            let (_, v) = replace(&mut self.buckets[index][pos], (key, value));
-            return Some(v);
-        }
-        self.buckets[index].push((key, value));
-
-        None
+        self.buckets.insert(key, value)
     }
 
     pub(super) fn contains_key(&self, key: &K) -> bool {
         self.get(key).is_some()
     }
 
-    pub(super) fn get(&self, key: &K) -> Option<&V> {
-        let index = self.eval_index(key);
-        self.buckets
-            .get(index)
-            .map(|l| l.iter().find(|(k, _)| k == key).map(|(_, v)| v))
-            .flatten()
+    pub(super) fn get<'a>(&'a self, key: &'a K) -> Option<&'a V> {
+        self.buckets.get(key)
     }
 
     pub(super) fn remove(&mut self, key: &K) -> Option<V> {
-        if self.get(key).is_some() {
-            let index = self.eval_index(&key);
-            let key_index = self.buckets[index].iter().position(|(k, _)| k == key)?;
-            let (_, v) = self.buckets[index].swap_remove(key_index);
-            return Some(v);
-        }
-        None
+        self.buckets.remove(key)
     }
 
-    // Amount of entries. Note: not `self.buckets.len()`, which is the same as cap in current implementation,
-    // nor the `self.non_empty_buckets()`
     pub(super) fn len(&self) -> usize {
-        self.buckets.iter().fold(0, |acc, l| acc + l.len())
+        self.buckets.count_items()
     }
 
     fn needs_resize(&self) -> bool {
-        let non_empty_buckets = self.non_empty_buckets();
-        if non_empty_buckets == 0 {
+        let real_len = self.buckets.len();
+        if real_len == 0 {
             return true;
         }
         // small occupancy reduces collision probability
-        let occupancy = non_empty_buckets / self.buckets.capacity();
+        let occupancy = real_len / self.buckets.cap();
         occupancy > 7
-    }
-
-    // This is strange at first glance. But the fact that we initialize bucket with default values by `resize_with(cap, || Vec::new())`
-    // means that we have `self.bucket.len() == self.bucket.cap()`. So the "real len" is an amount of non-empty buckets in `self.bucket` vector.
-    fn non_empty_buckets(&self) -> usize {
-        self.buckets.iter().filter(|l| !l.is_empty()).count()
     }
 
     // If we need to resize bucket vector to prevent collision,
@@ -101,21 +71,90 @@ where
     // so previous indexes become invalid.
     fn resize(&mut self) {
         // todo занеси в evernote. Намного лучше, чем cap = abssd(); if cap == 0 и блабла
-        let cap = match self.buckets.capacity() {
+        let cap = match self.buckets.cap() {
             // "0" case will be called on the first insert
             0 => INITIAL_LEN,
             n => n * 2,
         };
+        self.buckets.resize(cap)
+    }
+}
+
+impl<K: Hash + Eq, V> Buckets<K, V> {
+    fn new() -> Self {
+        // Initializing like this `vec![Vec::new(), some_cap] needs K to implement Clone`,
+        // but by doing current zero initialization we enlarge our trait bound.
+        Self(Vec::new())
+    }
+
+    fn remove(&mut self, key: &K) -> Option<V> {
+        let i = self.eval_index(&key);
+        // Deleting if entry exists by finding its index in `self.0[i]` bucket and returning value
+        if let Some(pos) = self.get_pos_in_bucket(i, key) {
+            let (_, v) = self.0[i].swap_remove(pos); // todo swap remove хорош, когда все равно на порядок внутри
+            return Some(v)
+        }
+        None
+    }
+
+    fn insert(&mut self, key: K, value: V) -> Option<V> {
+        let i = self.eval_index(&key);
+        // Checking if entry exists by finding its index in `self.0[i]` bucket and returning value
+        if let Some(pos) = self.get_pos_in_bucket(i, &key) {
+            let (_, v) = replace(&mut self.0[i][pos], (key, value));
+            return Some(v)
+        }
+        // Otherwise there aren't such keys and we add a new one
+        self.0[i].push((key, value));
+        None
+    }
+
+    fn get<'a>(&'a self, key: &'a K) -> Option<&'a V> {
+        let i = self.eval_index(key);
+        self.0
+            .get(i)
+            .map(|bucket| bucket.iter().find(|(k, _)| k == key))
+            .flatten()
+            .map(|(_, v)| v)
+    }
+
+    fn get_pos_in_bucket(&self, bucket: usize, key: &K) -> Option<usize> {
+        self.0
+            .get(bucket)
+            .map(|bucket| bucket.iter().position(|(k, _)| k == key))
+            .flatten()
+    }
+
+    fn cap(&self) -> usize {
+        self.0.capacity()
+    }
+
+    // This is strange at first glance. But the fact that we initialize bucket with default values by `resize_with(cap, || Vec::new())` at `Self::resize`
+    // means that we have `self.0.len() == self.0.cap()`. So the "real len" is an amount of non-empty buckets in `self.0` vector.
+    fn len(&self) -> usize {
+        self.0.iter().filter(|l| !l.is_empty()).count()
+    }
+
+    // Amount of entries. Note: not `self.0.len()`, which is the same as cap in current implementation,
+    // nor the `Self::count_non_empty()`
+    fn count_items(&self) -> usize {
+        self.0.iter().fold(0, |acc, l| acc + l.len())
+    }
+
+    fn resize(&mut self, cap: usize) {
         let mut new_bucket = Vec::with_capacity(cap);
         new_bucket.resize_with(cap, || Vec::new());
 
-        let old_bucket = replace(&mut self.buckets, new_bucket);
+        let old_buckets = replace(&mut self.0, new_bucket);
+        self.update_old_values(old_buckets);
+    }
 
+    fn update_old_values(&mut self, old_buckets: Vec<Vec<(K, V)>>) {
         // Fill in with old entries, but with new indexes.
-        for list in old_bucket {
-            let (k, _) = list.get(0).expect("existing list can't be empty");
+        for bucket in old_buckets {
+            let (k, _) = bucket.get(0).expect("existing bucket can't be empty");
             let new_index = self.eval_index(k);
-            self.buckets[new_index] = list;
+            self.0[new_index] = bucket;
         }
     }
 
@@ -125,7 +164,7 @@ where
         key.hash(&mut h);
         let hash = h.finish() as usize;
         // This is very important! `hash mod array_size`. In current implementation array_size == len == cap.
-        hash % self.buckets.len()
+        hash % self.0.len()
     }
 }
 
